b404444ec795dcb9a69a957f62e3282b762259c6
elenaescolin <elenaeslin@gmail.com>
2024-03-10 09:58:37 +0100

    feat: add new name on submit

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react"
+
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log(newName)
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+  }
+  
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
-    <>
-      
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
   )
 }
 
